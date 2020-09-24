07d8017e46e8df0568af80f6fcc8762122926e39
KishanItaliya <kishanp0210@gmail.com>
2020-09-24 15:47:56 +0530

    Implementing checkout

--- amazon-clone/src/Product.js
+++ amazon-clone/src/Product.js
@@ -1,16 +1,30 @@
 import React from 'react'
 import "./Product.css"
 
-function Product() {
+function Product({ title, image, price, rating }) {
     return (
         <div className="product">
             <div className="product__info">
-                <p>The lean startup</p>
+                <p>{title}</p>
                 <p className="product__price">
                     <small>$</small>
-                    <strong>19.99</strong>
+                    <strong>{price}</strong>
                 </p>
+                <div className="product__rating">
+                    {Array(rating)
+                        .fill()
+                        .map((_, i) => (
+                            <span role="img" aria-label="star">⭐</span>
+                        ))
+                    }
+                </div>
             </div>
+
+            <img src={image} alt="products logo" />
+            
+            <button>
+                Add to Basket
+            </button>
         </div>
     )
 }
---
+++ amazon-clone/src/Subtotal.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./Subtotal.css"
+
+function Subtotal() {
+    return (
+        <div className="subtotal">
+            <h2>The subtotal will go here</h2>
+        </div>
+    )
+}
+
+export default Subtotal
