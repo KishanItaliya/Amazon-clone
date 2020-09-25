3ee4f49e05873f0b588433cc2325e14684345a76
KishanItaliya <kishanp0210@gmail.com>
2020-09-25 20:35:11 +0530

    commit

--- amazon-clone/src/Checkout.js
+++ amazon-clone/src/Checkout.js
@@ -15,7 +15,7 @@ function Checkout() {
                     className="checkout__ad"
                     src="https://images-na.ssl-images-amazon.com/images/G/02/UK_CCMP/TM/OCC_Amazon1._CB423492668_.jpg"
                     alt="checkout__ad"
-                />
+                /> 
                 <div>
                     <h3>Hello, {!user ? 'Guest' : user.email}</h3>
                     <h2 className="checkout__title">
